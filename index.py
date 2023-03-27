229f5dbee34133ad930f6098c541243c704726ff
geometry-fandom <128089801+geometry-fandom@users.noreply.github.com>
2023-03-27 21:27:39 +0200

    Update index.py

--- index.py
+++ index.py
@@ -3,8 +3,8 @@ from pyrogram import Client, filters, sync
 from pyrogram.errors import FloodWait
 
 stop = False
-api_id = "Paste your App ID here"
-api_hash = "Paste your App Hash here"
+api_id = "Paste your API ID here"
+api_hash = "Paste your API Hash here"
 times = 0
 
 def check_for_number(str):
@@ -18,17 +18,22 @@ client = Client('user_login', api_id, api_hash)
 
 client.start()
 client.stop()
-print('Succesfully login into your account.')
+print('Succefull login')
 
 @client.on_message(filters.command('spam', prefixes="/") & filters.me)
 def message_handler(client, message):
     global stop
     global times
     args = message.text.split(' ')
+    try:
+        if args[1] in '1234567890':
+            if check_for_number(args[1]) == True: times = int(args[1])
+    except IndexError:
+        client.edit_message_text(message.chat.id, message.id, 'Use: /spam <amount of messages> <message>\nBy @shineforever2.')
+        args.append('None')
     if args[1] == 'stop':
         stop = True
-        client.send_message(message.chat.id, 'You succesfully disabled spam.')
-    if check_for_number(args[1]) == True: times = int(args[1])
+        client.send_message(message.chat.id, 'Succesfully disabled spam.')
     args.pop(0)
     args.pop(0)
     if times >= 1:
@@ -45,12 +50,8 @@ def message_handler(client, message):
                 sleep(e.x)
             times -= 1
 
-@client.on_message(filters.command('help', prefixes="/") & filters.me)
-def message_handler(client, message):
-    client.edit_message_text(message.chat.id, message.id, 'Use: /spam <amount of messages> <message>\nBy @shineforever2.')
-
 @client.on_message(filters.command('status', prefixes='/') & filters.me)
 def message_handler(client, message):
-    client.edit_message_text(message.chat.id, message.id, 'Bot Works✅...')
+    client.edit_message_text(message.chat.id, message.id, 'Bot Works✅')
 
 client.run()
